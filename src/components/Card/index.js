4359dc41acf30ce67044b8bf8309e3716a896a14
Tarilov Andrei <0567834@mail.ru>
2022-12-03 21:59:40 +0300

    props,css.modules

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,20 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+function Card(props) {
+  const onClickPlus = () => {};
   return (
-    <div className="card">
+    <div className={styles.card}>
       <div className="favorite">
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={onClickPlus}>
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
