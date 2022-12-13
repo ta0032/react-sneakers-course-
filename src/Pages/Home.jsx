11d81aca4909938fa80ff22b22ae2a1ad4889bcd
Tarilov Andrei <0567834@mail.ru>
2022-12-13 14:54:56 +0300

    Исправление бага с лого поиска

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -36,7 +36,7 @@ function Home({
           {searchValue ? `Поиск по запросу "${searchValue}"` : "Все кроссовки"}
         </h1>
         <div className="search-block d-flex">
-          <img src="/img/search.svg" alt="Search" />
+          <img src="img/search.svg" alt="Search" />
           {searchValue && (
             <img
               className="clear cu-p"
